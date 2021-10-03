bc636c203b117d5aec6c8e544b632d9f1343fe77
pittour <91750645+pittour@users.noreply.github.com>
2021-10-03 22:42:41 +0200

    del

--- bjrs.js
+++
@@ -1,7 +0,0 @@
-let i =0
-while (i<10) {
-    document.getElementById("bjr").appendChild(document.createElement("p")).innerHTML = "Bonjour";
-    ++i;
-}
-
-document.querySelector("#bjr :nth-child(5)").style.color = "red";
